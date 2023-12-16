f77ab8e168e617f1e3736209427f8c3e67d11505
adeeb123-crypto <waizadeeb@gmail.com>
2023-12-16 22:18:23 +0400

    new features >added payment page to scan page redirection

--- src/app/pay-page/pay-page.component.ts
+++ src/app/pay-page/pay-page.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-pay-page',
   standalone: true,
   imports: [],
   templateUrl: './pay-page.component.html',
-  styleUrl: './pay-page.component.scss'
+  styleUrl: './pay-page.component.scss',
 })
 export class PayPageComponent {
+  constructor(private router: Router) {}
 
+  @HostListener('click', ['$event.target'])
+  onClick(target: HTMLElement) {
+    // Set the type to HTMLElement
+    if (target && target.classList.contains('continue-to-payment-button')) {
+      this.continueToPayment();
+    }
+  }
+
+  continueToPayment(): void {
+    this.router.navigate(['/scanpage']);
+  }
 }
